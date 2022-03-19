0859e630423c7d903268ee740f4db6849ba5ea7f
Kamil Harasimowicz <kamil.harasimowicz@hq.home.net.pl>
2022-03-19 11:28:03 +0100

    leibniz improvement

--- src/algorithm/leibniz/index.js
+++ src/algorithm/leibniz/index.js
@@ -3,25 +3,24 @@ import bigDecimal from 'js-big-decimal';
 export default function (precision, attempts) {
   attempts = attempts ? attempts : BigInt(precision) * 1000000n;
   const limit = new Date().getTime() + 3600 * 1000;
-  let pi = new bigDecimal(1);
-  for (let i = 0n; i < attempts; ++i) {
-    const sign = i === 0n || i % 2n === 0n ? -1 : 1;
-    const nextDecimal = new bigDecimal(1)
+  let pi = new bigDecimal(4);
+  let minus = true;
+  for (let i = 2n; i < attempts * 2n + 2n; i+=2n) {
+    const nextDecimal = new bigDecimal(4)
       .divide(
-        new bigDecimal(3)
-          .add(new bigDecimal(i * 2n)),
-        precision * 2
+        new bigDecimal(i + 1n)
       );
-    if (sign === -1) {
+    if (minus) {
       pi = pi.subtract(nextDecimal);
     } else {
       pi = pi.add(nextDecimal);
     }
+    minus = !minus;
 
     if (limit < new Date().getTime()) {
       break;
     }
   }
 
-  console.log(pi.multiply(new bigDecimal(4)).round(precision).getValue());
+  console.log(pi.round(precision).getValue());
 };
